960b081d2afd684c4c369e9b74b60099a0a280f2
Elliot Blanchard <git@elliotblanchard.com>
2021-04-03 17:51:43 -0400

    Began RPC implementation

--- routes/getinfo.js
+++ routes/getinfo.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const rpc = require('json-rpc2')
+
+debugger
 
 /* GET network info listing. */
 router.get('/', function(req, res, next) {
+  const client = rpc.Client.$create(8232, 'http://192.168.1.158', 'samwellhouston', 'silversandyblocks')
+
+  client.call('getinfo', [], function (err, result){
+    if (err) {
+      console.error('RPC Error: ' + err.toString())
+    }
+    console.log('Result: ' + result)
+  });
+
   res.send('getinfo')
 });
 
