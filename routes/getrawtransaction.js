2d46541f68da51f5f6566020de3dec7a3a4f6926
Elliot Blanchard <git@elliotblanchard.com>
2021-04-06 08:52:40 -0400

    Added getrawtransaction call to RPC

--- routes/getrawtransaction.js
+++ routes/getrawtransaction.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const request = require('request')
+let rpcOptions = require('../modules/rpcOptions.js')
 
 /* GET raw transaction listing. */
 router.get('/', function(req, res, next) {
-  res.send('getrawtransaction')
+  const txid = req.query.txid
+
+  rpcOptions["body"] = JSON.stringify( {"jsonrpc": "1.0", "id": "api_request", "method": "getrawtransaction", "params": [txid, 1] })
+
+  request(rpcOptions, (error, response, body) => {
+    if (error) {
+        res.send(`An error has occurred: ${error}`)
+    } else {
+        const jsonResponse = JSON.parse(body) 
+        res.json(jsonResponse)
+    }
+  })
 });
 
 module.exports = router
+
+// zc.getrawtransaction(tx_hash.to_s, 1)
+// 7605b5cb54f4c4134e53ee3d45637a50ac18bc31b737cb73c033f80cc88f6ffb
+// 8daf402263d0ef504ed2808635980944aec38c0f6638ef5dd58e13a26e3d4a88
